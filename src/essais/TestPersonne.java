6bace3a31d416cba071f869c2468e2a3948e06be
Moulin Jordan <jordan.moulin72@gmail.com>
2018-07-18 10:36:49 +0200

    Tp 1 : modifs

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -12,6 +12,14 @@ public class TestPersonne {
 		p1.adresse.codePostal = 72230;
 		p1.adresse.ville = "Mulsanne";
 		p1.adresse.libelleVoie = "Rue du schtroumf";
+		
+		Personne p2= new Personne();
+		p2.prenom = "Ouba";
+		p2.nom = "Ouba";
+		p2.adresse.numeroRue = 1;
+		p2.adresse.codePostal = 1337;
+		p2.adresse.ville = "Tropico";
+		p2.adresse.libelleVoie = "Rue du oubaouba";
 
 	}
 
